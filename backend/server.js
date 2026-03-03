1a79b82be0e70324f015e6d6a60ee70742696d34
Felipe Santos <fe.campossantos@gmail.com>
2026-03-03 13:00:59 -0300

    updated to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -148,9 +148,6 @@ app.get("/download-raw", async (req, res) => {
   }
 });
 
-const HOST =
-  process.env.npm_lifecycle_event === "dev" ? "localhost" : "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
